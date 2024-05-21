0e9bcf8780c9c4aab01701535505026b6dace25d
BT_1N3_21 <sample@bths.edu>
2024-05-21 10:57:33 -0400

    started drawing outline for main tower, added MainTower class

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -32,13 +32,13 @@ public class GraphicsPanel extends JPanel implements KeyListener, MouseListener,
         g.drawRect(10, 540, 30, 40); //draws border for next card
         g.drawRect(0, 490, 500, 0); //draws divisor for cards selection and main game
 
-
+        g.drawRect(250, 30, 65, 65);
         g.drawRect(50, 70, 50, 50); //draws enemy tower 1
         g.drawRect(382, 70, 50, 50); //draws enemy tower 2
         g.drawRect(50, 370, 50, 50); //draws tower 1
         g.drawRect(382, 370, 50, 50); //draws tower 2
 
-        g.drawRect(433, 370, 50, 0); //measure to check for even distance from sides
+        g.drawRect(0, 10, 50, 0); //measure to check for even distance from sides
         g.drawRect(375, 420, 0, 70); //measure to check for even distance from top & bottom sides
 
         g.drawRect(0, 230,500, 30); //draws bridge
---
+++ src/MainTower.java
@@ -0,0 +1,14 @@
+public class MainTower {
+
+    private int towerHealth;
+    private int towerDamage;
+    private int attackSpeed;
+    private int attackRange;
+
+    public MainTower() {
+        towerHealth = 6000;
+        towerDamage = 150;
+        attackSpeed = 1000;
+    }
+
+}
